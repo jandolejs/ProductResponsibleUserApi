17c40a8f44e02fd97c7e88aabc26df2ca255a1e6
Jan Dolejš <jandolejs1999@gmail.com>
2022-08-04 13:45:37 +0200

    add grid

--- Api/Data/ProductResponsibleUserApiSearchResultsInterface.php
+++ Api/Data/ProductResponsibleUserApiSearchResultsInterface.php
@@ -7,20 +7,8 @@ declare(strict_types=1);
 
 namespace Aiti\ProductResponsibleUserApi\Api\Data;
 
-interface ProductResponsibleUserApiSearchResultsInterface extends \Magento\Framework\Api\SearchResultsInterface
+interface ProductResponsibleUserApiSearchResultsInterface extends \Magento\Framework\Api\Search\SearchResultInterface
 {
-
-    /**
-     * Get ProductResponsibleUser list.
-     * @return \Aiti\ProductResponsibleUserApi\Api\Data\ProductResponsibleUserApiInterface[]
-     */
     public function getItems();
-
-    /**
-     * Set user_id list.
-     * @param \Aiti\ProductResponsibleUserApi\Api\Data\ProductResponsibleUserApiInterface[] $items
-     * @return $this
-     */
-    public function setItems(array $items);
 }
 
